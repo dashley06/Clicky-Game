3c0799a61d4dc0997e59cebe840a1c8daf2b6021
dashley06 <debashley06@gmail.com>
2020-02-03 19:58:34 -0500

    styled cards, margins, positions

--- clicky-game/src/components/Card.js
+++ clicky-game/src/components/Card.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./Card.css";
 
 const Card = props => (
-        <div className="card img-container">
-            <img alt={props.name} src={props.image} id={props.id}
-            onClick={() => props.shuffleScoreCard(props.id)} />
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} id={props.id}
+                onClick={() => props.shuffleScoreCard(props.id)} />
+            </div>
         </div>
   );
   
